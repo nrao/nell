1003ffc8d65e279c3a4c2ec76988cecafd2e4089
Paul Marganian <pmargani@nrao.edu>
2009-04-08 10:48:02 -0400

    added the admin to urls.py

--- server/urls.py
+++ server/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls.defaults     import *
 from server.sesshuns.views import SessionResource, WindowResource, gen_opportunities
 
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+
 urlpatterns = patterns('',
    # ...
 #     url(r'^sessions(/(\d+)$)?$', SessionResource(permitted_methods=('GET', 'PUT', 'POST'))),
@@ -10,4 +15,5 @@ urlpatterns = patterns('',
    , url(r'^windows/(\d+)$',  WindowResource(permitted_methods=('PUT', 'GET', 'POST')))
    , url(r'^gen_opportunities$',        gen_opportunities)
    , url(r'^gen_opportunities/(\d+)$',  gen_opportunities)
+   , (r'^admin/(.*)', admin.site.root)
 )
