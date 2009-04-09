6d2b585e2f66db7471fb8cc8b8f4578a6ca44710
Paul Marganian <pmargani@nrao.edu>
2009-04-09 16:58:24 -0400

    added remaining Classes to admin

--- server/sesshuns/admin.py
+++ server/sesshuns/admin.py
@@ -1,11 +1,16 @@
-from server.sesshuns.models import Sesshun, Semester, Project_Type, Allotment, Project, Session_Type, Observing_Type, Receiver, Parameter
+from server.sesshuns.models import * 
 from django.contrib import admin
 
+class Receiver_GroupInline(admin.TabularInline):
+    model = Receiver_Group
+    extra = 1
+
 class SesshunAdmin(admin.ModelAdmin):
-    list_display = ['name', 'project', 'frequency', 'allotment']
+    list_display = ['name', 'project', 'frequency', 'allotment', 'receiver_list']
     list_filter = ['name',  'project']
     search_fields = ['name']
     #date_hierarchy = 'pub_date'
+    inlines = [Receiver_GroupInline]
 
 class SesshunInline(admin.TabularInline):
     model = Sesshun
@@ -42,6 +47,33 @@ class ReceiverAdmin(admin.ModelAdmin):
 class ParameterAdmin(admin.ModelAdmin):
     list_display = ['name', 'type']
 
+class CadenceAdmin(admin.ModelAdmin):
+    pass
+
+class Receiver_GroupAdmin(admin.ModelAdmin):
+    pass
+
+class Receiver_Group_ReceiverAdmin(admin.ModelAdmin):
+    pass
+
+class Observing_ParameterAdmin(admin.ModelAdmin):
+    pass
+
+class StatusAdmin(admin.ModelAdmin):
+    pass
+
+class WindowAdmin(admin.ModelAdmin):
+    pass
+
+class OpportunityAdmin(admin.ModelAdmin):
+    pass
+
+class SystemAdmin(admin.ModelAdmin):
+    pass
+
+class TargetAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Sesshun, SesshunAdmin)
 admin.site.register(Semester, SemesterAdmin)
 admin.site.register(Project_Type, Project_TypeAdmin)
@@ -51,4 +83,13 @@ admin.site.register(Session_Type, Session_TypeAdmin)
 admin.site.register(Observing_Type, Observing_TypeAdmin)
 admin.site.register(Receiver, ReceiverAdmin)
 admin.site.register(Parameter, ParameterAdmin)
+admin.site.register(Cadence, CadenceAdmin)
+admin.site.register(Receiver_Group, Receiver_GroupAdmin)
+admin.site.register(Receiver_Group_Receiver, Receiver_Group_ReceiverAdmin)
+admin.site.register(Observing_Parameter, Observing_ParameterAdmin)
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Window, WindowAdmin)
+admin.site.register(Opportunity, OpportunityAdmin)
+admin.site.register(System, SystemAdmin)
+admin.site.register(Target, TargetAdmin)
 
