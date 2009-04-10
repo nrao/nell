d161592ac209de6bc4b11bca521cd9b78e892705
Mark Clark <mclark@nrao.edu>
2009-04-10 05:45:30 -0400

    gave up on single-line urlpatterns

--- server/urls.py
+++ server/urls.py
@@ -8,7 +8,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
    # ...
-#     url(r'^sessions(/(\d+)$)?$', SessionResource(permitted_methods=('GET', 'PUT', 'POST'))),
      url(r'^sessions$',       SessionResource(permitted_methods=('GET', 'PUT', 'POST')))
    , url(r'^sessions/(\d+)$', SessionResource(permitted_methods=('PUT', 'GET', 'POST')))
    , url(r'^windows$',        WindowResource(permitted_methods=('GET', 'PUT', 'POST')))
