4d0fca13a94a3d210583f6037a84fd44fa286a41
Mark Clark <mclark@nrao.edu>
2009-04-29 20:18:02 -0400

    merged with Mike rev 193

--- test/edu/nrao/dss/client/ColumnDefinitionTest.java
+++ test/edu/nrao/dss/client/ColumnDefinitionTest.java
@@ -27,15 +27,15 @@ public class ColumnDefinitionTest extends GWTTestCase {
     assertNotNull(colDef);
     SessionColConfig sColConf = new SessionColConfig("test", "Test", 100, Integer.class);
     ColumnModel colMod = colDef.getColumnModel(sColConf);
-    assertEquals(colMod.getColumnCount(), 39);
-    assertEquals(colMod.getColumnHeader(1), "Source");
+    assertEquals(45, colMod.getColumnCount());
+    assertEquals("Source", colMod.getColumnHeader(1));
     assertTrue(colDef.getAllFieldNames().contains("Proj Code"));
     assertTrue(colDef.hasColumn(ColumnDefinition.TR_ERR_LIMIT));
     assertFalse(colDef.hasColumn("Pinky_Hippo"));
-    assertEquals(colDef.getColumn(ColumnDefinition.PRECIP).getHeader(), "Precip");
+    assertEquals("Precip", colDef.getColumn(ColumnDefinition.PRECIP).getHeader());
     //assertEquals(colDef.getValue( ??
-    assertEquals(colDef.getClasz("science"), ScienceField.class);
-    assertEquals(colDef.getField(ColumnDefinition.COORD_MODE).getOriginalValue(), null);
-    assertEquals(colDef.getField(ColumnDefinition.COORD_MODE).getFieldLabel(), ColumnDefinition.COORD_MODE);
+    assertEquals(ScienceField.class, colDef.getClasz("science"));
+    assertEquals(null, colDef.getField(ColumnDefinition.COORD_MODE).getOriginalValue());
+    assertEquals(ColumnDefinition.COORD_MODE, colDef.getField(ColumnDefinition.COORD_MODE).getFieldLabel());
   }
 }
