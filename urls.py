2e3f94e9d5edfeff6418e56dc040551d08b111ed
Paul Marganian <pmargani@nrao.edu>
2009-07-14 09:30:58 -0400

    Added periodJSON url to urls - TBF: rename

--- urls.py
+++ urls.py
@@ -17,5 +17,7 @@ urlpatterns = patterns(''
    , url(r'^period/form/(\d+)$',  period_form)
    , url(r'^period$',             PeriodResource(permitted_methods=('GET', 'PUT', 'POST')))
    , url(r'^period/(\d+)$',       PeriodResource(permitted_methods=('PUT', 'GET', 'POST')))
+   , url(r'^periodJSON$',             PeriodJSONResource(permitted_methods=('GET', 'PUT', 'POST')))
+   , url(r'^periodJSON/(\d+)$',       PeriodJSONResource(permitted_methods=('PUT', 'GET', 'POST')))
    , (r'^admin/',                 include(admin.site.urls))
 )
