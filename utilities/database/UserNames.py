7e368adae6d02d271f18ad77f5102f28ca242020
Paul Marganian <pmargani@nrao.edu>
2009-08-26 12:10:08 -0400

    added more methods to UserNames to facilitate reconciling usernames & PST IDs

--- utilities/database/UserNames.py
+++ utilities/database/UserNames.py
@@ -9,18 +9,104 @@ import lxml.etree as ET
 
 class UserNames(object):
 
-#    def __init__(self, host = "trent.gb.nrao.edu"
-#                     , user = "dss"
-#                     , passwd = "asdf5!"
-#                     , database = "dss_pmargani_test"
-#                     , silent   = True
-#                 ):
-#        self.db = m.connect(host   = host
-#                          , user   = user
-#                          , passwd = passwd
-#                          , db     = database
-#                            )
-#        self.cursor = self.db.cursor()
+    """
+    This class is simply a collection of different methods for managing
+    issues related to usernames and IDs.
+    """
+
+    def confirmUserInfo(self, queryUser, queryPassword):
+        "Checks contents of User table against info from PST service w/ pst ID."
+
+        users = User.objects.all()
+        ui = UserInfo()
+
+        # keep records
+        noPstId = []
+        matched = []
+        mismatched = []
+
+        for u in users:
+            if u.pst_id is not None:
+                info = ui.getStaticContactInfoByID(u.pst_id
+                                                 , queryUser
+                                                 , queryPassword)
+            
+                # extract what the PST thinks about this user
+                pstId        = int(info['id'])
+                pstUsername  = info['account-info']['account-name'].strip()
+                pstFirstName = info['name']['first-name'].strip()
+                pstLastName  = info['name']['last-name'].strip()
+
+                assert (pstId == u.pst_id)
+                assert (pstUsername == u.username)
+                # just check for names
+                #if pstId != u.pst_id or pstUsername != u.username or \
+                if pstFirstName != u.first_name or \
+                   pstLastName != u.last_name:
+                    print "PST: %s %s vs. DSS: %s %s" % (pstFirstName
+                                                       , pstLastName
+                                                       , u.first_name
+                                                       , u.last_name)
+                    mismatched.append(u)
+                else:
+                    matched.append(u)
+            else:
+                noPstId.append(u)
+
+        # report
+        print "len(users): ", len(users)
+        print "len(noPstId): ", len(noPstId)
+        print "len(matched): ", len(matched)
+        print "len(mismatched): ", len(mismatched)
+
+    def transferUserNamesByOriginalID(self):
+        """
+        What if you already have a DB that already has usernames & IDs 
+        correctly reconciled?  Use this script to transfer that information
+        to the settings.py DB.
+        NOTE: simply using an SQL dump to transfer the User table from one
+        DB to the other might be risky, since 
+        users are linked by primary key to projects, etc.
+        """
+
+        # reading from another Postgres DB is a pain, so just read in 
+        # a text dump of the sanctioned table
+        filename = "user_table.txt"
+        f = open(filename, 'r')
+        lines = f.readlines()
+
+        # keep some records
+        matched = []
+        mismatched = []
+
+        for line in lines:
+            parts = line.split('\t')
+            # columns: (id, original_id, pst_id, username, sanctioned, 
+            # first_name, last_name, contact_instructions, role_id) 
+            original_id = int(parts[1])
+            pst_id      = int(parts[2])
+            username    = parts[3]
+            first_name  = parts[5]
+            last_name   = parts[6]
+            # use the original id to map
+            ourUser = first(User.objects.filter(original_id=original_id).all())
+            # does this make sense?
+            if first_name != ourUser.first_name or \
+               last_name != ourUser.last_name:
+                print "mismatch: ", ourUser, first_name, last_name
+                mismatched.append((ourUser, first_name, last_name))   
+                ourUser.first_name = first_name
+                ourUser.last_name = last_name
+            # pass over the usernames and ids:
+            matched.append(ourUser)
+            ourUser.username = username
+            ourUser.pst_id   = pst_id
+            ourUser.save()
+    
+        print "mismatched: ", mismatched
+        print "len(mismatched): ", len(mismatched)
+        print "len(matched): ", len(matched)
+        print "len(ourUsers): ", len(User.objects.all())
 
     def getUserNamesFromIDs(self, queryUser, queryPassword):
 
@@ -30,6 +116,10 @@ class UserNames(object):
         # get all users
         users = User.objects.all()
 
+        missing = []
+        saved = []
+        agree = []
+
         # for each user, get their static contact info
         for user in users:
 
@@ -37,6 +127,7 @@ class UserNames(object):
 
             if id is None:
                 print "skipping user, no pst_id: ", user
+                missing.append(user)
                 continue
 
             # save off the username
@@ -48,14 +139,24 @@ class UserNames(object):
                 if user.username == username:
                     #no-op
                     print "usernames agree for: ", user
+                    agree.append(user)
                 else:
                     raise "user.username != username! " + user.username + "!=" + username
             else:
+                saved.append(user)
                 print "saving username: ", user, username
                 user.username = username
                 user.save()
-                
+        
+        print "saved: ", saved
+        print "missing: ", missing
+
+        print "num agreed: ", len(agree)
+        print "num saved: ", len(saved)
+        print "num no pst_id: ", len(missing)
+
     def getUserNames(self, username, password):
+        "DEPRECATED: but may be useful for testing query services"
 
         skipping = []
 
