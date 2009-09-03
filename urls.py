1f5e344cb4934b49e3bf445878444222207d8b10
Mark Clark <mclark@nrao.edu>
2009-09-03 08:39:03 -0400

    commented local-only urls

--- urls.py
+++ urls.py
@@ -8,6 +8,8 @@ from sesshuns.observers            import *
 from django.contrib import admin
 admin.autodiscover()
 
+# Note a blank comment attached to the url denotes local access only
+
 urlpatterns = patterns(''
    , url(r'^search$',                                      search)
    , url(r'^profile/(\d+)/blackout$',                      blackout)
@@ -23,17 +25,16 @@ urlpatterns = patterns(''
    , url(r'^project/([^/]+)/investigator/(\d+)/observer$', toggle_observer)
    , url(r'^project/([^/]+)/investigator/(\d+)/priority$', modify_priority)
    , url(r'^project/([^/]+)/session/([^/]+)/enable$',      toggle_session)
-   , url(r'^projects$',           ProjectResource(permitted_methods=('GET', 'PUT', 'POST')))
-   , url(r'^projects/(\d+)$',     ProjectResource(permitted_methods=('PUT', 'GET', 'POST')))
-   , url(r'^sessions/options$',   get_options)
-   , url(r'^sessions$',           SessionResource(permitted_methods=('GET', 'PUT', 'POST')))
-   , url(r'^sessions/(\d+)$',     SessionResource(permitted_methods=('PUT', 'GET', 'POST')))
-   , url(r'^receivers/schedule$', receivers_schedule)
-   , url(r'^periods$',            PeriodResource(permitted_methods=('GET', 'PUT', 'POST')))
-   , url(r'^periods/(\d+)$',      PeriodResource(permitted_methods=('PUT', 'GET', 'POST')))
+   , url(r'^projects$',           ProjectResource(permitted_methods=('GET', 'PUT', 'POST'))) #
+   , url(r'^projects/(\d+)$',     ProjectResource(permitted_methods=('PUT', 'GET', 'POST'))) #
+   , url(r'^sessions/options$',   get_options) #
+   , url(r'^sessions$',           SessionResource(permitted_methods=('GET', 'PUT', 'POST'))) #
+   , url(r'^sessions/(\d+)$',     SessionResource(permitted_methods=('PUT', 'GET', 'POST'))) #
+   , url(r'^receivers/schedule$', receivers_schedule) #
+   , url(r'^periods$',            PeriodResource(permitted_methods=('GET', 'PUT', 'POST'))) #
+   , url(r'^periods/(\d+)$',      PeriodResource(permitted_methods=('PUT', 'GET', 'POST'))) #
    , (r'^admin/',                 include(admin.site.urls))
    , (r'^accounts/login/$', 'django_cas.views.login')
    , (r'^accounts/logout/$', 'django_cas.views.logout')
-   , (r'^static/(?P<path>.*)$', 'django.views.static.serve',
-        {'document_root': settings.STATIC_DOC_ROOT})
+   , (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_DOC_ROOT})
 )
