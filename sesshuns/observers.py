f83bec01fea0ff02c25f1e57121bf7cb52510af9
Amy Shelton <ashelton@nrao.edu>
2009-09-03 22:48:38 -0400

    Reformatted blackouts to remove seconds. Changed search page to display appropriate text when no results found.

--- sesshuns/observers.py
+++ sesshuns/observers.py
@@ -173,14 +173,18 @@ def search(request, *args, **kws):
     user   = first(User.objects.filter(username = loginUser))
     assert user is not None
     search   = request.POST.get('search', '')
+
     projects = Project.objects.filter(
         Q(pcode__icontains = search) | \
             Q(name__icontains = search) | \
             Q(semester__semester__icontains = search))
+    projects = [p for p in projects]
     projects.extend([p for p in Project.objects.all() \
                      if p.pcode.replace("0", "").replace("-", "").replace("GBT", "") == search])
-    users    = User.objects.filter(
+
+    users = User.objects.filter(
         Q(first_name__icontains = search) | Q(last_name__icontains = search))
+
     return render_to_response("sesshuns/search.html"
                             , {'ps' : projects
                              , 'us' : users
