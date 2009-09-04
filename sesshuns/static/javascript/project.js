0bdcd34547c3f879cc7de4139342103065b81245
Amy Shelton <ashelton@nrao.edu>
2009-09-04 13:24:53 -0400

    Changed format of time in project calendar events.

--- sesshuns/static/javascript/project.js
+++ sesshuns/static/javascript/project.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 	$('#calendar').fullCalendar({
         draggable: false,
+        timeFormat: 'H:i',
         events: event_url,
         unavailable: unavailable_url
 	});
