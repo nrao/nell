d09e023880aa6e274810166cd9626f783d7020e1
Amy Shelton <ashelton@nrao.edu>
2009-10-26 16:14:52 -0400

    Updates to project report per Karen and Toney

--- utilities/database/ProjectReport.py
+++ utilities/database/ProjectReport.py
@@ -10,11 +10,16 @@ from sets            import Set
 def get_type(project):
     return Set([s.session_type.type for s in project.sesshun_set.all()])
 
-def get_sessions(project, typ, enabled = True, complete = False):
-    return [s for s in project.sesshun_set.all() \
-             if s.session_type.type ==  typ and \
-                s.status.enabled == enabled and \
-                s.status.complete == complete]
+def get_sessions(project, typ, enabled = None, complete = False):
+    if enabled is not None:
+        return [s for s in project.sesshun_set.all() \
+                 if s.session_type.type ==  typ and \
+                    s.status.enabled == enabled and \
+                    s.status.complete == complete]
+    else:
+        return [s for s in project.sesshun_set.all() \
+                 if s.session_type.type ==  typ and \
+                    s.status.complete == complete]
 
 def get_rcvrs(project, typ):
     rcvr_dict = {
@@ -49,45 +54,47 @@ def get_rcvrs(project, typ):
 def get_allotment_hours(project, typ):
     return sum([s.allotment.total_time for s in get_sessions(project, typ)])
 
-def get_accounting_hours(project, typ):
+def get_obs_hours(project, typ):
     return sum([
-        p.accounting.not_billable + \
-        p.accounting.other_session_weather + \
-        p.accounting.other_session_rfi + \
-        p.accounting.other_session_other + \
-        p.accounting.lost_time_weather + \
-        p.accounting.lost_time_rfi + \
-        p.accounting.lost_time_other - \
-        p.accounting.scheduled
+        p.accounting.scheduled - \
+        p.accounting.not_billable - \
+        p.accounting.other_session_weather - \
+        p.accounting.other_session_rfi - \
+        p.accounting.other_session_other - \
+        p.accounting.lost_time_weather - \
+        p.accounting.lost_time_rfi - \
+        p.accounting.lost_time_other
     for s in get_sessions(project, typ) for p in s.period_set.all()])
 
-def get_hours(project,typ):
-    return sum([
-        get_allotment_hours(project, typ)
-      , get_accounting_hours(project, typ)
-    ])
+def get_rem_hours(project,typ):
+    return get_allotment_hours(project, typ) - get_obs_hours(project, typ)
 
 def GenerateProjectReport():
     outfile = open("./DssProjectReport.txt", 'w')
 
-    outfile.write("  Project   | Sessions | Complete |   Type   | Incomplete | Rem Hours | Not Enabled | Observers | Blackout | Backup | Receivers\n")
-    outfile.write("-------------------------------------------------------------------------------------------------------------------------------\n")
+    outfile.write("  Project   |   Type   | Sessions | Not Enabled | Observers | Complete | Incomplete | Obs Hrs | Rem Hrs | Blackout | Backup | Receivers\n")
 
+    count           = 0
     sorted_projects = sorted(Project.objects.filter(complete = False)
                            , lambda x, y: cmp(x.pcode, y.pcode))
     for p in sorted_projects:
         for typ in get_type(p):
+            if count % 10 == 0:
+                outfile.write("-------------------------------------------------------------------------------------------------------------------------------------------\n")
+            count += 1
+
             outfile.write(
-                "%s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s\n" % \
+                "%s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s | %s\n" % \
                 (p.pcode.ljust(11)
-               , str(len(p.sesshun_set.all())).center(8)
-               , str(len([s for s in p.sesshun_set.all() \
-                            if s.status.complete == True])).center(8)
                , typ.center(8)
-               , str(len(get_sessions(p, typ))).center(10)
-               , str(get_hours(p,typ)).center(9)
+               , str(len(p.sesshun_set.all())).center(8)
                , str(len(get_sessions(p, typ, False))).center(11)
                , str(len(p.get_observers())).center(9)
+               , str(len([s for s in p.sesshun_set.all() \
+                            if s.status.complete == True])).center(8)
+               , str(len(get_sessions(p, typ))).center(10)
+               , str(get_obs_hours(p,typ)).center(7)
+               , str(get_rem_hours(p,typ)).center(7)
                , str(any([len(o.user.blackout_set.all()) == 0 \
                           for o in p.get_observers()])).center(8)
                , str(any([s.status.backup for s in p.sesshun_set.all()])).center(6)
