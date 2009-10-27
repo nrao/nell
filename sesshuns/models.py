12e9f351fdf0db283407996826a7f650db5302ff
Paul Marganian <pmargani@nrao.edu>
2009-10-27 12:16:48 -0400

    added Reservation class so that this table can be populated with reservation info, for use by Antioch, whose BOS query service still is not working (TBF)

--- sesshuns/models.py
+++ sesshuns/models.py
@@ -1716,12 +1716,11 @@ class Project_Blackout_09B(models.Model):
         # problems with postrgreSQL
         db_table = "project_blackouts_09b"
 
-# TBF: might need this in order to get around Haskell not working w/ BOS
-#class Reservation(models.Model):
-#    user       = models.ForeignKey(User)
-#    start_date = models.DateTimeField(help_text = "yyyy-mm-dd hh:mm:ss")
-#    end_date   = models.DateTimeField(help_text = "yyyy-mm-dd hh:mm:ss")
-#
-#    class Meta:
-#        db_table = "reservations"
+class Reservation(models.Model):
+    user       = models.ForeignKey(User)
+    start_date = models.DateTimeField(help_text = "yyyy-mm-dd hh:mm:ss")
+    end_date   = models.DateTimeField(help_text = "yyyy-mm-dd hh:mm:ss")
+
+    class Meta:
+        db_table = "reservations"
     
