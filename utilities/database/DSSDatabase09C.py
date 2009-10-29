38a02b0b3966f0cc08bc7b0d0179f578216a5fd5
Amy Shelton <ashelton@nrao.edu>
2009-10-29 14:41:43 -0400

    Updated receiver schedule

--- utilities/database/DSSDatabase09C.py
+++ utilities/database/DSSDatabase09C.py
@@ -59,9 +59,9 @@ class DSSDatabase09C(DSSDatabase):
         rcvrs = ['L', 'C', 'K', 'X', 'Ku', 'S', 'Hol', 'Q', '600'] 
         rcvrChanges.append((dt, rcvrs))
 
-        # Nov 4: C -> MBA
+        # Nov 4: C -> MBA, 600 -> 342
         dt = datetime(2009, 11, 4, 16)
-        rcvrs = ['L', 'K', 'X', 'Ku', 'MBA', 'S', 'Hol', 'Q', '600'] 
+        rcvrs = ['L', 'K', 'X', 'Ku', 'MBA', 'S', 'Hol', 'Q', '342'] 
         rcvrChanges.append((dt, rcvrs))
 
         # Nov 12: 600 -> 800
