5957eb4ede2760048535cffd0a95d52d024bd44a
Paul Marganian <pmargani@nrao.edu>
2009-11-23 12:03:52 -0500

    added ProjTimeAcctReport to make it easy to get a report on the time accounting for a given project

---
+++ utilities/database/ProjTimeAcctReport.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+from django.core.management import setup_environ
+import settings
+setup_environ(settings)
+
+from sesshuns.models      import Project, first
+from tools.TimeAccounting import TimeAccounting
+import sys
+
+def GenerateProjectTimeAccountingReport(pcode):
+
+    project = first(Project.objects.filter(pcode = pcode)) 
+    if project is None:
+        print "cannot find project: ", pcode
+        return
+    ta = TimeAccounting()
+    ta.report(project, "%sTimeAccounting.txt" % pcode)
+
+if __name__ == '__main__':
+    print sys.argv
+    if len(sys.argv) > 1:
+        pcode = sys.argv[1]
+        print pcode
+        GenerateProjectTimeAccountingReport(pcode)
+    else:
+        print "must provide project code"
