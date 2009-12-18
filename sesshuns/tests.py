7f3b3a2aec99f746dc78e339821031368534ad4a
Paul Marganian <pmargani@nrao.edu>
2009-12-18 06:43:44 -0500

    refactored DSSPrime2DSS to also support incremental updates from dss_prime (tranfer_only_new top level funciton), and expanded unit tests.

--- sesshuns/tests.py
+++ sesshuns/tests.py
@@ -6,6 +6,7 @@ from django.test.client  import Client
 from django.http         import QueryDict
 import simplejson as json
 import lxml.etree as et
+import MySQLdb as mysql
 
 from models                          import *
 from test_utils.NellTestCase         import NellTestCase
@@ -1545,8 +1546,7 @@ class TestDBReporter(NellTestCase):
 
 class TestDSSPrime2DSS(NellTestCase):
 
-    def test_transfer(self):
-
+    def assert_DB_empty(self):
         # make sure our DB is almost blank 
         projects = Project.objects.all()
         self.assertEquals(1, len(projects))
@@ -1555,7 +1555,45 @@ class TestDSSPrime2DSS(NellTestCase):
         users = User.objects.all()
         self.assertEquals(0, len(users))
 
+    def compare_users(self, dbname):
+
+        old = []
+        new = []
+        fold, fnew = self.compare_users_worker(dbname, "friends", old, new)
+        pold, pnew = self.compare_users_worker(dbname, "authors", fold, fnew)
+        return (pold, pnew)
+
+    def compare_users_worker(self, dbname, table, old_users, new_users):
+        
+        db = mysql.connect(host   = "trent.gb.nrao.edu"
+                     , user   = "dss"
+                     , passwd = "asdf5!"
+                     , db     = dbname
+                            )
+        cursor = db.cursor()
+        
+        query = "SELECT * FROM %s" % table
+        cursor.execute(query)
+        
+        for row in cursor.fetchall():
+            idcol = 3 if table == "friends" else 4 
+            id = int(row[idcol])
+            user = first(User.objects.filter(original_id = id).all())
+            if user is None:
+                if id not in new_users:
+                    new_users.append(id)
+            else:
+                if id not in old_users:
+                    old_users.append(id)
+        
+        return (old_users, new_users)
+
+    def test_transfer(self):
+ 
+        self.assert_DB_empty()
+
         t = DSSPrime2DSS(database = 'dss_prime_unit_tests')
+        t.quiet = True
         t.transfer()
 
         # now test what we've got to make sure the transfer worked:
@@ -1605,7 +1643,46 @@ class TestDSSPrime2DSS(NellTestCase):
         users = User.objects.all()
         self.assertEquals(287, len(users))
 
+    def test_transfer_only_new_1(self):
 
+        self.assert_DB_empty()
+
+        # populate into an empty DB: not much of a test, really!
+        t = DSSPrime2DSS(database = 'dss_prime_unit_tests_2')
+        t.quiet = True
+        t.transfer_only_new()
+
+        self.assertEquals( 53, len(t.new_projects))
+        self.assertEquals(  0, len(t.old_projects))
+        self.assertEquals(121, len(t.new_sessions))
+        self.assertEquals(  0, len(t.old_sessions))
+        self.assertEquals(199, len(t.new_users))
+        self.assertEquals(  0, len(t.old_users))
+
+    def test_transfer_only_new_2(self):
+
+        self.assert_DB_empty()
+
+        # populate the DB w/ 09C and earlier stuff
+        t = DSSPrime2DSS(database = 'dss_prime_unit_tests')
+        t.quiet = True
+        t.transfer()
+
+        # now that our model DB is initialized, predict how many new and
+        # old users reside in our new source DB.
+        old_users, new_users = self.compare_users('dss_prime_unit_tests_2')
+
+        # now make sure we transfer only the new 10A stuff
+        t = DSSPrime2DSS(database = 'dss_prime_unit_tests_2')
+        t.quiet = True
+        t.transfer_only_new()
+
+        self.assertEquals( 53, len(t.new_projects))
+        self.assertEquals(  0, len(t.old_projects))
+        self.assertEquals(121, len(t.new_sessions))
+        self.assertEquals(  0, len(t.old_sessions))
+        self.assertEquals(len(new_users), len(t.new_users)) 
+        self.assertEquals(len(old_users), len(t.old_users))
 
 class TestReceiverCompile(NellTestCase):
 
