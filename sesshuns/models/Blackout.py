f6beb3161ab099849a2342bb7f31d6a747f08e9a
Amy Shelton <ashelton@nrao.edu>
2010-05-17 13:37:34 -0400

    Fixed bug in isActive. Forgot that endate can be None.

--- sesshuns/models/Blackout.py
+++ sesshuns/models/Blackout.py
@@ -25,7 +25,7 @@ class Blackout(models.Model):
         if self.start_date is None:
             return False # Never started, not active
         
-        if self.repeat.repeat == "Once" and self.end_date <= date:
+        if self.repeat.repeat == "Once" and self.end_date and self.end_date <= date:
             return False # Done
 
         if self.start_date >= date:
