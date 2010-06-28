cc7a5ac50db8eaf02b74026c2a890d4677d0f1cc
Mike McCarty <mmccarty@nrao.edu>
2010-06-27 22:48:14 -0400

    script for populating auth_user fk

--- populate_auth_user.sql
+++ populate_auth_user.sql
@@ -5,6 +5,7 @@ INSERT INTO auth_user VALUES (DEFAULT, 'dperera', '', '', 'dperera@nrao.edu', '!
 INSERT INTO auth_user VALUES (DEFAULT, 'jbraatz', '', '', 'jbraatz@nrao.edu', '!', 't', 't', 't', '2009-09-08 17:04:40-04', '2009-09-08 17:04:40-04');
 INSERT INTO auth_user VALUES (DEFAULT, 'jharnett', '', '', 'jharnett@nrao.edu', '!', 't', 't', 't', '2009-09-08 17:04:40-04', '2009-09-08 17:04:40-04');
 INSERT INTO auth_user VALUES (DEFAULT, 'koneil', '', '', 'koneil@nrao.edu', '!', 't', 't', 't', '2009-09-08 17:04:40-04', '2009-09-08 17:04:40-04');
+INSERT INTO auth_user VALUES (DEFAULT, 'mmccarty', '', '', 'mmccarty@nrao.edu', '!', 't', 't', 't', '2009-09-08 17:04:40-04', '2009-09-08 17:04:40-04');
 INSERT INTO auth_user VALUES (DEFAULT, 'mclark', '', '', 'mclark@nrao.edu', '!', 't', 't', 't', '2009-09-08 17:04:40-04', '2009-09-08 17:04:40-04');
 INSERT INTO auth_user VALUES (DEFAULT, 'pmargani', '', '', 'pmargani@nrao.edu', '!', 't', 't', 't', '2009-09-08 17:04:40-04', '2009-09-08 17:04:40-04');
 INSERT INTO auth_user VALUES (DEFAULT, 'tminter', '', '', 'tminter@nrao.edu', '!', 't', 't', 't', '2009-09-08 17:04:40-04', '2009-09-08 17:04:40-04');
