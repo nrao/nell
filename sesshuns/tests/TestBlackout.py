c0062f179dcc9c371aeb5b1286e96e7e0502d806
Paul Marganian <pmargani@nrao.edu>
2010-11-16 16:02:38 -0500

    fixed bug in Blackout.generateDates where non-repeating blackout dates were getting passed the range specified

--- sesshuns/tests/TestBlackout.py
+++ sesshuns/tests/TestBlackout.py
@@ -90,8 +90,16 @@ class TestBlackout(NellTestCase):
         gdts = self.blackout1.generateDates(calstart, calend)
         self.assertEquals(dts, gdts)
 
+        # should be none in June.
+        calstart = datetime(2009, 6, 1)
+        calend   = datetime(2009, 6, 30)
+        gdts = self.blackout1.generateDates(calstart, calend)
+        self.assertEquals(0, len(gdts))
+
         # repeats are more complicated
         # how does January look?
+        calstart = datetime(2009, 1, 1)
+        calend   = datetime(2009, 1, 30)
         start = self.blackout2.start_date
         end = self.blackout2.end_date
         dts = [(start, end)]
@@ -125,6 +133,8 @@ class TestBlackout(NellTestCase):
         calend   = datetime(2008, 10, 30)
         gdts = self.blackout3.generateDates(calstart, calend)
         self.assertEquals(dts, gdts)
+
+        # TBF: test filter outside of blackouts????
 
     def test_projectBlackout(self):
         "Repeat some of the other tests, but for project blackouts"
