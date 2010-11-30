823ef8c9ee5f3c08f2f1cb1dc10e2a95edb26d75
Mike McCarty <mmccarty@nrao.edu>
2010-11-30 10:30:49 -0500

    calculator: added frequency resolution sql init script

---
+++ calculator/db_init_sql/FrequencyResolution.sql
@@ -0,0 +1,8 @@
+--Spectral Processor (SP)
+insert into calculator_frequency_resolution values (default, 9, null, 1024);
+
+--Auto Correlating Spectrometer (ACS)
+insert into calculator_frequency_resolution values (default, 10, 800, 8192);
+insert into calculator_frequency_resolution values (default, 10, 200, 32768);
+insert into calculator_frequency_resolution values (default, 10, 50,  131072);
+insert into calculator_frequency_resolution values (default, 10, 125, 131072);
