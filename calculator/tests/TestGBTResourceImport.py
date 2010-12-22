7ce117f8e3fb6ebeeef109ff065fca80fe222978
Mike McCarty <mmccarty@nrao.edu>
2010-12-22 11:01:58 -0500

    calculator: GBT Resource Import script

---
+++ calculator/tests/TestGBTResourceImport.py
@@ -0,0 +1,15 @@
+from calculator.utilities  import GBTResourceImport
+
+import unittest
+
+class TestGBTResourceImport(unittest.TestCase):
+
+    def test_init(self):
+        resources = GBTResourceImport("calculator/gbt_resources_table.txt")
+        for c in resources.configs:
+            for k, vs in c.iteritems():
+                for v in vs: 
+                    self.assertNotEqual((k, None), (k, v))
+
+if __name__ == "__main__":
+    unittest.main()
