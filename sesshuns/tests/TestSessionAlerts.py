c4c1d521239aad562671f9ec9e8ce3610b98495e
Mike McCarty <mmccarty@nrao.edu>
2011-01-10 15:16:36 -0500

    finished session alerts

---
+++ sesshuns/tests/TestSessionAlerts.py
@@ -0,0 +1,215 @@
+from test_utils.NellTestCase import NellTestCase
+from nell.utilities.database import SessionAlerts
+from sesshuns.models         import *
+from sesshuns.httpadapters   import *
+from utils                   import create_sesshun
+from TestSessionAlertBase    import TestSessionAlertBase
+
+class TestSessionAlerts(TestSessionAlertBase):
+
+    def setUp(self):
+        super(TestSessionAlerts, self).setUp()
+
+    def testFindDisabledWindowedSessionAlerts(self):
+        self.makeWindowedSession(start_date = date(2009, 4, 5)
+                               , duration   = 7
+                                )
+
+        sa = SessionAlerts.SessionAlerts()
+        w  = first(Window.objects.all())
+        w.session.status.enabled = False
+        w.session.status.save()
+
+        # Find alerts 20 days before window
+        alerts = sa.findDisabledSessionAlerts(now = w.start_datetime() - timedelta(days = 20))
+        self.assertEqual(len(alerts), 0)
+
+        # Find alerts 10 days before window
+        alerts = sa.findDisabledSessionAlerts(now = w.start_datetime() - timedelta(days = 10))
+        self.assertEqual(len(alerts), 1)
+        self.assertEqual(alerts[0].id, 1)
+
+        # Find alerts at the start of the window
+        alerts = sa.findDisabledSessionAlerts(now = w.start_datetime())
+        self.assertEqual(len(alerts), 1)
+        self.assertEqual(alerts[0].id, 1)
+
+        # Find alerts at the default period
+        alerts = sa.findDisabledSessionAlerts(now = w.default_period.start)
+        self.assertEqual(len(alerts), 1)
+        self.assertEqual(alerts[0].id, 1)
+
+        # Find alerts after the default period, should be zero
+        alerts = sa.findDisabledSessionAlerts(now = w.default_period.start + timedelta(days = 10))
+        self.assertEqual(len(alerts), 0)
+
+    def testFindDisabledWindowedSessionAlertsCritical(self):
+        """
+            Critical being within 4 days of the window.
+        """
+        self.makeWindowedSession(start_date = date(2009, 4, 5)
+                               , duration   = 7
+                                )
+
+        sa = SessionAlerts.SessionAlerts()
+        sa.stageBoundary = 4
+        w  = first(Window.objects.all())
+        w.session.status.enabled = False
+        w.session.status.save()
+
+        # Find alerts 10 days before window
+        alerts = sa.findDisabledSessionAlerts(now = w.start_datetime() - timedelta(days = 10))
+        self.assertEqual(len(alerts), 0)
+
+        # Find alerts 4 days before window
+        alerts = sa.findDisabledSessionAlerts(now = w.start_datetime() - timedelta(days = 4))
+        self.assertEqual(len(alerts), 1)
+        self.assertEqual(alerts[0].id, 1)
+
+        # Find alerts at the start of the window
+        alerts = sa.findDisabledSessionAlerts(now = w.start_datetime())
+        self.assertEqual(len(alerts), 1)
+        self.assertEqual(alerts[0].id, 1)
+
+        # Find alerts at the default period
+        alerts = sa.findDisabledSessionAlerts(now = w.default_period.start)
+        self.assertEqual(len(alerts), 1)
+        self.assertEqual(alerts[0].id, 1)
+
+        # Find alerts after the default period, should be zero
+        alerts = sa.findDisabledSessionAlerts(now = w.default_period.start + timedelta(days = 4))
+        self.assertEqual(len(alerts), 0)
+
+    def testFindDisabledElectiveSessionAlerts(self):
+        self.makeElectiveSession() 
+
+        sa = SessionAlerts.SessionAlerts()
+        e  = first(Elective.objects.all())
+        e.session.status.enabled = False
+        e.session.status.save()
+        start, end = e.periodDateRange()
+
+        # Find alerts 20 days before window
+        alerts = sa.findDisabledSessionAlerts(now = start - timedelta(days = 20))
+        self.assertEqual(len(alerts), 0)
+
+        # Find alerts 10 days before window
+        alerts = sa.findDisabledSessionAlerts(now = start - timedelta(days = 10))
+        self.assertEqual(len(alerts), 1)
+        self.assertEqual(alerts[0].id, 1)
+
+        # Find alerts at the start of the window
+        alerts = sa.findDisabledSessionAlerts(now = start)
+        self.assertEqual(len(alerts), 1)
+        self.assertEqual(alerts[0].id, 1)
+
+        # Find alerts at the end of the elective
+        alerts = sa.findDisabledSessionAlerts(now = end)
+        self.assertEqual(len(alerts), 1)
+        self.assertEqual(alerts[0].id, 1)
+
+        # Find alerts after the end of the elective, should be zero
+        alerts = sa.findDisabledSessionAlerts(now = end + timedelta(days = 10))
+        self.assertEqual(len(alerts), 0)
+
+    def testFindDisabledElectiveSessionAlertsCritical(self):
+        """
+            Critical being within 4 days of the elective.
+        """
+        self.makeElectiveSession() 
+
+        sa = SessionAlerts.SessionAlerts()
+        sa.stageBoundary = 4
+        e  = first(Elective.objects.all())
+        e.session.status.enabled = False
+        e.session.status.save()
+        start, end = e.periodDateRange()
+
+        # Find alerts 10 days before window
+        alerts = sa.findDisabledSessionAlerts(now = start - timedelta(days = 10))
+        self.assertEqual(len(alerts), 0)
+
+        # Find alerts 4 days before window
+        alerts = sa.findDisabledSessionAlerts(now = start - timedelta(days = 4))
+        self.assertEqual(len(alerts), 1)
+        self.assertEqual(alerts[0].id, 1)
+
+        # Find alerts at the start of the window
+        alerts = sa.findDisabledSessionAlerts(now = start)
+        self.assertEqual(len(alerts), 1)
+        self.assertEqual(alerts[0].id, 1)
+
+        # Find alerts at the end of the elective
+        alerts = sa.findDisabledSessionAlerts(now = end)
+        self.assertEqual(len(alerts), 1)
+        self.assertEqual(alerts[0].id, 1)
+
+        # Find alerts after the end of the elective, should be zero
+        alerts = sa.findDisabledSessionAlerts(now = end + timedelta(days = 10))
+        self.assertEqual(len(alerts), 0)
+
+    def testFindDisabledFixedSessionAlerts(self):
+        self.makeFixedSession() 
+
+        sa = SessionAlerts.SessionAlerts()
+        p  = first(Period.objects.filter(session__session_type__type = 'fixed'))
+        p.session.status.enabled = False
+        p.session.status.save()
+        start = p.start
+
+        # Find alerts 20 days before fixed period
+        alerts = sa.findDisabledSessionAlerts(now = start - timedelta(days = 20))
+        self.assertEqual(len(alerts), 0)
+
+        # Find alerts 10 days before fixed period
+        alerts = sa.findDisabledSessionAlerts(now = start - timedelta(days = 10))
+        self.assertEqual(len(alerts), 1)
+        self.assertEqual(alerts[0].id, 1)
+
+        # Find alerts at the start of the fixed period
+        alerts = sa.findDisabledSessionAlerts(now = start)
+        self.assertEqual(len(alerts), 1)
+        self.assertEqual(alerts[0].id, 1)
+
+        # Find alerts after the fixed period, should be zero
+        alerts = sa.findDisabledSessionAlerts(now = start + timedelta(days = 10))
+        self.assertEqual(len(alerts), 0)
+
+    def testFindDisabledFixedSessionAlertsCritical(self):
+        self.makeFixedSession() 
+
+        sa = SessionAlerts.SessionAlerts()
+        sa.stageBoundary = 4
+        p  = first(Period.objects.filter(session__session_type__type = 'fixed'))
+        p.session.status.enabled = False
+        p.session.status.save()
+        start = p.start
+
+        # Find alerts 10 days before fixed period
+        alerts = sa.findDisabledSessionAlerts(now = start - timedelta(days = 10))
+        self.assertEqual(len(alerts), 0)
+
+        # Find alerts 4 days before fixed period
+        alerts = sa.findDisabledSessionAlerts(now = start - timedelta(days = 4))
+        self.assertEqual(len(alerts), 1)
+        self.assertEqual(alerts[0].id, 1)
+
+        # Find alerts at the start of the fixed period
+        alerts = sa.findDisabledSessionAlerts(now = start)
+        self.assertEqual(len(alerts), 1)
+        self.assertEqual(alerts[0].id, 1)
+
+        # Find alerts after the fixed period, should be zero
+        alerts = sa.findDisabledSessionAlerts(now = start + timedelta(days = 4))
+        self.assertEqual(len(alerts), 0)
+
+    def xtestRaiseAlerts(self):
+        self.makeFixedSession() 
+
+        sa = SessionAlerts.SessionAlerts(stageBoundary = 4)
+        p  = first(Period.objects.filter(session__session_type__type = 'fixed'))
+        p.session.status.enabled = False
+        p.session.status.save()
+        start = p.start
+
+        sa.raiseAlerts(now = start - timedelta(days = 4))
---
+++ utilities/database/SessionAlerts.py
@@ -0,0 +1,125 @@
+from django.core.management import setup_environ
+import settings
+setup_environ(settings)
+
+from sesshuns.models         import *
+from utilities.SessionAlertNotifier import *
+
+class SessionAlerts(object):
+
+    def __init__(self, quiet = True, filename = None, stageBoundary = 15):
+
+        self.stageBoundary = stageBoundary
+        self.now = datetime.utcnow()
+        
+        # for reporting results
+        self.quiet = quiet
+        self.filename = filename if filename is not None else "SessionAlerts.txt"
+        self.reportLines = []
+       
+    def findDisabledSessionAlerts(self, now = None):
+        now   = now if now is not None else datetime.utcnow()
+        today = datetime(now.year, now.month, now.day)
+
+        def withinWindowBoundary(w):
+            daysTillStart   = abs((w.start_datetime() - today).days)
+            return (daysTillStart <= self.stageBoundary and now < w.default_period.start) \
+                   or (now >= w.start_datetime() and now <= w.default_period.start)
+
+        retval  = [w for w in Window.objects.filter(complete = False
+                                                  , session__status__enabled = False)
+                     if withinWindowBoundary(w)]
+
+        def withinElectiveBoundary(e):
+            start, end    = e.periodDateRange()
+            daysTillStart = abs((start - today).days)
+            return (daysTillStart <= self.stageBoundary and now <= end)
+
+        retval.extend([e for e in Elective.objects.filter(complete = False
+                                                        , session__status__enabled = False)
+                         if withinElectiveBoundary(e)])
+
+        def withinFixedBoundary(p):
+            start = p.start
+            daysTillStart = abs((start - today).days)
+            return daysTillStart <= self.stageBoundary and now <= start
+
+        retval.extend([p for p in Period.objects.filter(session__session_type__type = 'fixed'
+                                                      , state__name = 'Pending'
+                                                      , session__status__enabled = False)
+                         if withinFixedBoundary(p)])
+
+        return retval
+
+    def getRange(self, unknown):
+        return SessionAlertEmail.getRange(unknown)
+
+    def add(self, lines):
+        "For use with printing reports"
+        if not self.quiet:
+            print lines
+        self.reportLines += lines
+
+    def write(self):        
+        "For use with printing reports"
+        # write it out
+        if self.filename is not None:
+            f = open(self.filename, 'w')
+            f.writelines(self.reportLines)
+            f.close()
+
+    def raiseAlertsDSSTeam(self, now = None, test = False, quiet = True):
+
+        self.stageBoundary = 4
+        self.quiet = quiet
+        for unknown in self.findDisabledSessionAlerts(now):
+            
+            # report this
+            self.add("Alert for %s session # %d\n" % (unknown.session.session_type.type
+                                                    , unknown.session.id))
+            
+            sa = SessionAlertNotifier(unknown = unknown
+                                    , test = test)
+            
+            # for now, *really* play it safe
+            if not test:
+                if sa.email is not None:
+                    self.add("Notifying DSS Team about disabled %s session # %d: %s\n" % \
+                         (unknown.session.session_type.type
+                        , unknown.session.id
+                        , sa.email.GetRecipientString()))
+                #print sa.email.GetText()
+                sa.notify()
+
+        self.write()
+        self.stageBoundary = 15
+
+    def raiseAlerts(self, now = None, test = False, quiet = True):
+
+        self.quiet = quiet
+        for unknown in self.findDisabledSessionAlerts(now):
+            
+            # report this
+            self.add("Alert for %s session # %d\n" % (unknown.session.session_type.type
+                                                    , unknown.session.id))
+            
+            sa = SessionAlertNotifier(unknown = unknown
+                                    , test = test
+                                    , type = "disabled_observers")
+            
+            # for now, *really* play it safe
+            if not test:
+                if sa.email is not None:
+                    self.add("Notifying observers about disabled %s session # %d: %s\n" % \
+                         (unknown.session.session_type.type
+                        , unknown.session.id
+                        , sa.email.GetRecipientString()))
+                #print sa.email.GetText()
+                sa.notify()
+        
+        self.write()
+
+if __name__ == "__main__":
+    sa = SessionAlerts()
+    sa.raiseAlerts()
+    sa.raiseAlertsDSSTeam()
