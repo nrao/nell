b71f44909b1b7e6dc8b94f6fde47b79e74880cac
Paul Marganian <pmargani@nrao.edu>
2011-03-01 16:01:40 -0500

    added unit test for Utilitie's project_search method.

--- sesshuns/tests/TestUtilities.py
+++ sesshuns/tests/TestUtilities.py
@@ -91,4 +91,39 @@ class TestUtilities(BenchTestCase):
         self.assertEqual([emails[1][0], emails[2][0]], ob)
         self.assertEqual(emails[1], fs)
 
-               
+    def test_project_search(self):
+
+        # create some projects
+        pt = first(Project_Type.objects.all())
+        sem10a = Semester.objects.get(semester = "10A")
+        p1 = Project(pcode = "GBT10A-001"
+                   , semester = sem10a
+                   , name = "Greatest Project Ever"
+                   , project_type = pt
+                    )
+        p1.save()            
+        p2 = Project(pcode = "GBT10A-002"
+                   , semester = sem10a
+                   , name = "Suckiest Project Ever"
+                   , project_type = pt
+                    )
+        p2.save()            
+        allProjs = Project.objects.all()
+
+        # look for them
+        projs = project_search("")
+        self.assertEqual(len(allProjs), len(projs))
+        projs = project_search("GBT10A-001")
+        self.assertEqual(1,  len(projs))
+        self.assertEqual(p1, projs[0])
+        projs = project_search("GBT10A-002")
+        self.assertEqual(1,  len(projs))
+        self.assertEqual(p2, projs[0])
+        projs = project_search("10A")
+        self.assertEqual(2,  len(projs))
+        projs = project_search("Suck")
+        self.assertEqual(1,  len(projs))
+        self.assertEqual(p2, projs[0])
+        projs = project_search("10A02")
+        self.assertEqual(1,  len(projs))
+        self.assertEqual(p2, projs[0])
