de87a9815aef332bb7aa9dcfef3392322b0b90fa
Mike McCarty <mmccarty@nrao.edu>
2011-03-25 16:19:13 -0400

    corrected misspelling

--- utilities/database/DSSPrime2DSS.py
+++ utilities/database/DSSPrime2DSS.py
@@ -106,7 +106,7 @@ class DSSPrime2DSS(object):
         self.report_transfer_only_new()
 
         # also print out the mimic of carl's report
-        dbr = DBReporter(filename = "DBReport.xt", quiet = self.quiet)
+        dbr = DBReporter(filename = "DBReport.txt", quiet = self.quiet)
         dbr.reportProjectSummaryByPcode(self.new_projects)
 
     def set_sanctioned_flags(self):
