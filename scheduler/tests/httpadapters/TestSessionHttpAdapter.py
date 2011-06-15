004613e8cde116f1543cfa5b3264b4ecfc1e726f
Paul Marganian <pmargani@nrao.edu>
2011-06-15 12:50:57 -0400

    Added infrastructure for supporting the Tr Err Limit parameter, via Sesshun.get_tracking_error_threshold(), and updated SessionHttpAdapter to include this.

--- scheduler/tests/httpadapters/TestSessionHttpAdapter.py
+++ scheduler/tests/httpadapters/TestSessionHttpAdapter.py
@@ -10,6 +10,73 @@ class TestSessionHttpAdapter(NellTestCase):
         self.s       = create_sesshun()
         self.adapter = SessionHttpAdapter(self.s)
 
+    def test_jsondict(self):
+
+        json = self.adapter.jsondict()
+        exp = {'req_max': 6.0
+             , 'grade': 4.0
+             , 'nighttime': False
+             , 'transit': False
+             , 'sem_time': 0.0
+             , 'lst_in': ''
+             , 'id': 1L
+             , 'guaranteed': True
+             , 'lst_ex': ''
+             , 'source': u'test source'
+             , 'pcode': u'GBT09A-001'
+             , 'authorized': True
+             , 'between': '0'
+             , 'type': u'open'
+             , 'req_min': 2.0
+             , 'total_time': 3.0
+             , 'coord_mode': u'J2000'
+             , 'handle': u'Low Frequency With No RFI (GBT09A-001) 0'
+             , 'complete': False
+             , 'project_complete': 'No'
+             , 'source_h': 1.0
+             , 'source_v': 2.2999999999999998
+             , 'trk_err_threshold': 0.20000000000000001
+             , 'PSC_time': 2.0
+             , 'freq': 6.0
+             , 'name': 'Low Frequency With No RFI'
+             , 'science': u'pulsar'
+             , 'orig_ID': 0
+             , 'enabled': True
+             , 'remaining': 3.0
+             , 'xi_factor': 1.0
+             , 'receiver': ''
+             , 'backup': False}
+        self.assertEquals(exp, json)
+
+    def test_udpate_tr_err_threshold_obs_param(self):
+
+        # check inital state
+        th = self.adapter.sesshun.get_tracking_error_threshold_param()
+        self.assertEquals(None, th)
+        th = self.adapter.sesshun.get_tracking_error_threshold()
+        self.assertEquals(0.2, th)
+
+        # no-op
+        fdata = {}
+        old_value = self.adapter.sesshun.get_tracking_error_threshold_param()
+        self.adapter.update_tr_err_threshold_obs_param(fdata, old_value)
+        th = self.adapter.sesshun.get_tracking_error_threshold()
+        self.assertEquals(0.2, th)
+
+        # add the new param
+        fdata = {'trk_err_threshold' : 0.5}
+        old_value = self.adapter.sesshun.get_tracking_error_threshold_param()
+        self.adapter.update_tr_err_threshold_obs_param(fdata, old_value)
+        th = self.adapter.sesshun.get_tracking_error_threshold()
+        self.assertEquals(0.5, th)
+
+        # change the param
+        fdata = {'trk_err_threshold' : 0.6}
+        old_value = self.adapter.sesshun.get_tracking_error_threshold_param()
+        self.adapter.update_tr_err_threshold_obs_param(fdata, old_value)
+        th = self.adapter.sesshun.get_tracking_error_threshold()
+        self.assertAlmostEquals(0.6, th, 1)
+
     def test_update_lst_parameters(self):
         error = False
         try:
