b6e27ba78d04c50554a2850c695818557c6ac8d4
Paul Marganian <pmargani@nrao.edu>
2011-10-27 09:00:50 -0400

    Fixed TestObservers.test_event unit test that got broke when we added a classname to the Peirod eventjson.

--- users/tests/TestObservers.py
+++ users/tests/TestObservers.py
@@ -443,7 +443,7 @@ class TestObservers(TestObserversBase):
         response = self.get('/project/%s/events' % self.p.pcode, data)
         self.failUnlessEqual(response.status_code, 200)
         exp = [{"className": "blackout", "start": "2011-08-03T00:00:00+00:00", "end": "2011-08-08T00:00:00+00:00", "title": "Dana Balser: blackout"}
-             , {"start": "2011-08-04T00:00:00+00:00", "end": "2011-08-04T03:00:00", "title": "Observing Low Frequency With No RFI"}
+             , {"className": "period", "start": "2011-08-04T00:00:00+00:00", "end": "2011-08-04T03:00:00", "title": "Observing Low Frequency With No RFI"}
              , {"className": "semester", "start": "2012-02-01T00:00:00", "title": "Start of 12A"}
              , {"className": "semester", "start": "2012-08-01T00:00:00", "title": "Start of 12B"}
              ]
