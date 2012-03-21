f4e799315c2e5929994275d2fcf41fa08af94f4f
Paul Marganian <pmargani@nrao.edu>
2012-03-21 06:31:19 -0400

    Got out of import hell by NOT importing SemesterTimeAccounting in pht/tools/__init__.py; but how does everything still work when NOT importing this?

--- pht/tools/__init__.py
+++ pht/tools/__init__.py
@@ -1,3 +1,5 @@
-from SemesterTimeAccounting import SemesterTimeAccounting
+# TBF: this import causes import hell for other modules,
+# but why does everything work w/ it commented out?
+#from SemesterTimeAccounting import SemesterTimeAccounting
 from Sun                    import Sun
 from WindowStatistics       import WindowStatistics
