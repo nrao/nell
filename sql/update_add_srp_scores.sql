872a6eb06f4bb408f2c8bb2451a7b11662093548
Mike McCarty <mmccarty@nrao.edu>
2012-03-22 13:23:31 -0400

    added sql for updating the proposal table with new srp columns

---
+++ sql/update_add_srp_scores.sql
@@ -0,0 +1,3 @@
+alter table pht_proposals add column "normalizedSRPScore" float;
+alter table pht_proposals add column "draft_normalizedSRPScore" float;
+alter table pht_proposals add column "num_refs" integer;
