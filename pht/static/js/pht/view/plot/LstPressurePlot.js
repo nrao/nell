c26273a02d7d8af6f71656b97510f8389eafc748
Paul Marganian <pmargani@nrao.edu>
2012-03-26 15:45:15 -0400

    Finally figured out how to get all four plots to share common code in the parent class.

---
+++ pht/static/js/pht/view/plot/LstPressurePlot.js
@@ -0,0 +1,82 @@
+Ext.define('PHT.view.plot.LstPressurePlot', {
+    extend: 'Ext.chart.Chart',
+    alias: 'widget.lstpressureplot',
+    bodyStyle: {
+        background: '#fff',
+    }, 
+    width: 300,
+    height: 400,
+    store: 'LstPressures',
+    animate: true,
+    shadow: true,
+    legend: {
+        position: 'right',
+        labelFont: '6px Helvetica, sans-serif',
+    },  
+
+    initComponent: function() {
+        console.log('initComponent plot');
+        var me = this;
+        this.axes = [{
+            // X-Axis:
+            // Using Numeric has some funny consequences.  Since
+            // we always know the Ra is between 0 and 23, this is a 
+            // sensible axis type to use.
+            type: 'Category',
+            position: 'bottom',
+            grid: true,
+            fields: ['LST'],
+            title: 'LST',
+            label: {
+                font: '7px Helvetica, sans-serif',
+                rotate: {
+                    degrees: 315
+                }
+            }
+          },{
+            // Y-Axis:
+            type: 'Numeric',
+            position: 'left',
+            fields: this.pressureFields,
+            grid: true,
+            title: 'Pressure (Hrs)',
+            label: {
+                font: '7px Helvetica, sans-serif',
+            }
+        }];
+        this.series = [{
+            // Pressure data
+            type: 'column',
+            stacked: true,
+            xField: 'LST',
+            yField: this.pressureFields,
+            tips: {
+              trackMouse: true,
+              width: 300,
+              height: 80,
+              scope: this,
+              renderer: function(storeItem, item) {
+                // display any non zero values in the tooltip
+                // for each column
+                var values = [];
+                for (f in me.pressureFields) {
+                    var field = me.pressureFields[f];
+                    var value = storeItem.get(field);
+                    if (value > 0.0) {
+                        values.push(' ' + field + ' = ' + value.toFixed(3));
+                    }    
+                }
+                this.setTitle('LST = ' + storeItem.get('LST') + ': ' + values.join());
+              },
+            },  
+        },{
+            // Availability data
+            type: 'line',
+            showMarkers: false,
+            xField: 'LST',
+            yField: this.availableField,
+        }];
+        this.callParent();
+    },
+});
+
--- pht/static/js/pht/view/plot/LstPressureTotal.js
+++ pht/static/js/pht/view/plot/LstPressureTotal.js
@@ -1,5 +1,13 @@
-// map to colors 
-var fields = ['Carryover', 
+Ext.require('PHT.view.plot.PlotTheme');
+
+Ext.define('PHT.view.plot.LstPressureTotal', {
+    extend: 'PHT.view.plot.LstPressurePlot',
+    theme: 'LstPressureThemeTotal',
+    alias: 'widget.lstpressuretotal',
+
+    initComponent: function() {
+        // map to colors 
+        this.pressureFields = ['Carryover', 
               'Poor_A',
               'Poor_B',
               'Poor_C',
@@ -9,80 +17,10 @@ var fields = ['Carryover',
               'Excellent_A',
               'Excellent_B',
               'Excellent_C',
-              ];
-
-Ext.require('PHT.view.plot.PlotTheme');
-
-Ext.define('PHT.view.plot.LstPressureTotal', {
-    extend: 'PHT.view.plot.LstPressurePlot',
-    theme: 'LstPressureThemeTotal',
-    alias: 'widget.lstpressuretotal',
-    /*
-    bodyStyle: {
-        background: '#fff',
-    }, 
-    title: 'Lst Pressures',
-    width: 300,
-    height: 250,
-    store: 'LstPressures',
-    animate: true,
-    shadow: true,
-    legend: {
-        position: 'right',
+        ];        
+        this.availableField = 'Available';
+        this.callParent();
     },
-    */
-    axes: [{
-        type: 'Numeric',
-        position: 'left',
-        fields: fields,
-        grid: true,
-        title: 'Pressure (Hrs)',
-        minimum: 0,
-        //adjustMinimumByMajorUnit: 0
-    }, {
-        // Using Numeric has some funny consequences.  Since
-        // we always know the Ra is between 0 and 23, this is a 
-        // sensible axis type to use.
-        type: 'Category',
-        position: 'bottom',
-        grid: true,
-        fields: ['LST'],
-        title: 'LST',
-        label: {
-            rotate: {
-                degrees: 315
-            }
-        }
-    }],
-    series: [{
-        type: 'column',
-        stacked: true,
-        xField: 'LST',
-        yField: fields,
-        tips: {
-          trackMouse: true,
-          width: 300,
-          height: 80,
-          renderer: function(storeItem, item) {
-            // display any non zero values in the tooltip
-            // for each column
-            var values = [];
-            for (f in fields) {
-                var field = fields[f];
-                var value = storeItem.get(field);
-                if (value > 0.0) {
-                    values.push(' ' + field + ' = ' + value);
-                }    
-            }
-            this.setTitle('LST = ' + storeItem.get('LST') + ': ' + values.join());
-          },
-        },  
-    },{
-        type: 'line',
-        showMarkers: false,
-        xField: 'LST',
-        yField: 'Available',
-    }],
 
 });
 
