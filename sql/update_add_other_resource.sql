bebf0906c1a018f03cc14421f45b55419239ccfd
Paul Marganian <pmargani@nrao.edu>
2012-04-18 15:08:04 -0400

    added sql/update_add_other_resource for the new other fields.

---
+++ sql/update_add_other_resource.sql
@@ -0,0 +1,3 @@
+ALTER TABLE pht_sessions ADD COLUMN other_receiver character varying(2000);               
+ALTER TABLE pht_sessions ADD COLUMN other_backend character varying(2000);               
+
