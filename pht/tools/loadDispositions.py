c857253af404051257cd1f87dcfd38c4278ded74
Paul Marganian <pmargani@trent.(none)>
2013-05-22 13:58:04 -0400

    Updated loadDispostions to a) avoid using the mail commands in the first line of the disposition file b) avoid accented chars.

--- pht/tools/loadDispositions.py
+++ pht/tools/loadDispositions.py
@@ -3,10 +3,12 @@ import settings
 setup_environ(settings)
 
 from scheduler.models import Project
+from pht.tools.database.PstInterface import PstInterface
 import sys, os
 
 
 def loadDispositions(path):
+    pst = PstInterface()
     badPcodes = []
     for file in os.listdir(path):
         pcode, _ = file.split('.')
@@ -14,7 +16,9 @@ def loadDispositions(path):
         try:
             project = Project.objects.get(pcode = pcode)
             dspFile = open(path + '/' + file, 'r')
-            disposition = ''.join(dspFile.readlines())
+            # watch out for accents in the code, and don't read the first line
+            ls = [pst.safeUnicode(l) for l in dspFile.readlines()[1:]]
+            disposition = ''.join(ls)
             dspFile.close()
             project.disposition = disposition
             project.save()
