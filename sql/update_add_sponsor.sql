adc01eaa05fbd11985b25ba4426213004ee3c700
Paul Marganian <pmargani@trent.(none)>
2013-08-22 08:24:28 -0400

    updated update_add_sponsor.sql to also create the sponsor table.

--- sql/update_add_sponsor.sql
+++ sql/update_add_sponsor.sql
@@ -1,3 +1,31 @@
+--
+-- Name: sponsors; Type: TABLE; Schema: public; Owner: dss; Tablespace: 
+--
+
+CREATE TABLE sponsors (
+    id integer NOT NULL,
+    name character varying(150) NOT NULL,
+    abbreviation character varying(64) NOT NULL
+);
+
+
+ALTER TABLE public.sponsors OWNER TO dss;
+
+--
+-- Name: sponsors_id_seq; Type: SEQUENCE; Schema: public; Owner: dss
+--
+
+CREATE SEQUENCE sponsors_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.sponsors_id_seq OWNER TO dss;
+
+
 ALTER TABLE pht_proposals ADD COLUMN sponsor_id integer;
 
 CREATE INDEX pht_proposals_sponsor_id ON pht_proposals USING btree (sponsor_id);
