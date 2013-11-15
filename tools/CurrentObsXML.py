3f3150ecf8165ed219ad88261e914b8c71b4d15f
Paul Marganian <pmargani@trent.(none)>
2013-11-15 14:30:31 -0500

    First commit for the prototype of the EPO 'whats obsering now' project. Not exposing the url yet to keep this from being exposed to the public.

---
+++ tools/CurrentObsXML.py
@@ -0,0 +1,174 @@
+# Copyright (C) 2011 Associated Universities, Inc. Washington DC, USA.
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# 
+# Correspondence concerning GBT software should be addressed as follows:
+#       GBT Operations
+#       National Radio Astronomy Observatory
+#       P. O. Box 2
+#       Green Bank, WV 24944-0002 USA
+
+from django.core.management import setup_environ
+import settings
+setup_environ(settings)
+
+from lxml                        import etree
+from StringIO                    import StringIO
+from datetime                    import datetime, timedelta
+from utilities.database.external import GbtStatusDB
+from scheduler.models            import *
+
+class CurrentObsXML:
+
+    """
+    This class is responsible for gathering information on what is 
+    currently observing on the GBT, and providing this information
+    in a valid XML format, as specified by a schema.
+    """
+
+    def __init__(self, schema = None):
+
+        self.schema = schema if schema is not None else self.getDefaultSchema()
+
+        self.statusDB = GbtStatusDB()
+
+    def getXMLString(self):
+        "Returns (status, XMLString) of current observations"
+        info = self.getCurrentObsInfo()
+        xmlDoc = self.getXMLDoc(info)
+        # validate
+        print etree.tostring(xmlDoc)
+        if not self.validate(xmlDoc):
+           return (False, None)
+
+        return (True, etree.tostring(xmlDoc))
+           
+
+    def getXMLDoc(self, info):
+        "Returns xml tree based off dict of current obs. info"
+        root = etree.Element("currently_observing", telescope="GBT")
+        # shucks, order might matter
+        keys = ["proposal_code", "proposal_title", "proposal_abstract", "PI_name", "PI_institution", "source_name", "source_ra", "source_dec"]
+        #for key, value in info.items():
+        for key in keys:
+            subel = etree.Element(key)
+            subel.text = info[key]
+            root.append(subel)
+        return root
+
+    def validate(self, xmlDoc):
+        "Is the given xml valid according to our schema?"
+
+        # first, prepare the schema
+        f = StringIO(self.schema)
+        xmlschema_doc = etree.parse(f)
+        xmlschema = etree.XMLSchema(xmlschema_doc)
+
+        # prepare the xmlDoc
+        xmlStr = etree.tostring(xmlDoc)
+        xmlStrIo = StringIO(xmlStr)
+        doc = etree.parse(xmlStrIo)
+
+        # and validate
+        return xmlschema.validate(doc)
+
+    def getCurrentObsInfo(self, project = None, now = None):
+        "Returns the desired info in a dict."
+
+        # get the current project observing
+        if project is None:
+            # get the project that's observing now
+            if now is None:
+                now = datetime.now()
+            # first cast a wide net
+            ps = Period.objects.filter(start__lt = now + timedelta(days=1), start__gt = now - timedelta(days=1), state__name = 'Scheduled').order_by('start')
+            # now get the one you want
+            ps = [p for p in ps if p.start < now and p.end() > now] 
+            # TBF: check?
+            p = ps[0]
+            project = p.session.project
+
+           
+        # get all you can from DSS DB
+        pcode = project.pcode
+        title = project.name
+        abstract = project.abstract
+        pi = project.principal_investigator()
+        piInst = "unknown"
+        piName = "unknown"
+        if pi is not None:
+            piName = pi.display_name() 
+            uInfo = pi.getStaticContactInfo()
+            if len(uInfo['affiliations']) > 0:
+                piInst = uInfo['affiliations'][0]
+
+
+        # get the rest of other places (gbtstatus)
+        srcName, major, minor, epoch = self.statusDB.getSourceInfo()
+        ra = dec = "unknown"
+        if epoch == 'J2000':
+            # get rid of the (Ra/Dec) part
+            ra  = major.split(' ')[0]
+            dec = minor.split(' ')[0]
+
+        return dict(proposal_code=pcode
+                  , proposal_title=title
+                  , proposal_abstract=abstract
+                  , PI_name=piName
+                  , PI_institution=piInst
+                  , source_name=srcName
+                  , source_ra=ra
+                  , source_dec=dec
+                   )
+
+    def getDefaultSchema(self):
+        sch = """<?xml version="1.0" encoding="UTF-8"?>
+        <xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
+        
+         <xs:element name="currently_observing">
+          <xs:complexType>
+           <xs:sequence>
+           <xs:element name="proposal_code" type="xs:string"/>
+           <xs:element name="proposal_title" type="xs:string" minOccurs="0"/>
+           <xs:element name="proposal_abstract" type="xs:string" minOccurs="0"/>
+           <xs:element name="PI_name" type="xs:string" minOccurs="0"/>
+           <xs:element name="PI_institution" type="xs:string" minOccurs="0"/>
+           <xs:element name="source_name" type="xs:string" minOccurs="0"/>
+           <xs:element name="source_ra" type="xs:double"/>
+           <xs:element name="source_dec" type="xs:double"/>
+           </xs:sequence>
+           <xs:attribute name="telescope" type="xs:string"/>
+          </xs:complexType>
+         </xs:element>
+        
+         <xs:simpleType name="telescopes">
+          <xs:restriction base="xs:string">
+           <xs:enumeration value="ALMA"/>
+           <xs:enumeration value="VLA"/>
+           <xs:enumeration value="VLBA"/>
+           <xs:enumeration value="GBT"/>
+           <xs:enumeration value="other"/>
+          </xs:restriction>
+         </xs:simpleType>
+        
+        </xs:schema>        
+        """
+        return sch
+
+if __name__ == '__main__':
+
+    co = CurrentObsXML()
+    xmlStr = co.getXMLString()
+    print xmlStr
---
+++ users/tests/TestCurrentObsXML.py
@@ -0,0 +1,69 @@
+# Copyright (C) 2011 Associated Universities, Inc. Washington DC, USA.
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# 
+# Correspondence concerning GBT software should be addressed as follows:
+#       GBT Operations
+#       National Radio Astronomy Observatory
+#       P. O. Box 2
+#       Green Bank, WV 24944-0002 USA
+
+from lxml             import etree
+from test_utils       import NellTestCase
+from tools            import CurrentObsXML
+from scheduler.models import Project
+
+class TestCurrentObsXML(NellTestCase):
+
+    def test_getCurrentObsInfo(self):
+
+       co = CurrentObsXML.CurrentObsXML()
+
+       p = Project.objects.all()[0]
+       p.name = 'my title'
+       p.abstract = 'my abstract'
+       p.save()
+
+       info = co.getCurrentObsInfo(p)
+       exp = {'source_name': 'srcname'
+            , 'proposal_title': u'my title'
+            , 'proposal_abstract': 'my abstract'
+            , 'PI_institution': 'unknown'
+            , 'source_dec': '0.0'
+            , 'PI_name': 'unknown'
+            , 'proposal_code': u'GBT09A-001'
+            , 'source_ra': '0.0'
+             }
+       self.assertEqual(exp, info)     
+
+    def test_getXMLDoc(self):
+
+       co = CurrentObsXML.CurrentObsXML()
+       info = {'source_name': 'srcname'
+            , 'proposal_title': u'my title'
+            , 'proposal_abstract': 'my abstract'
+            , 'PI_institution': 'unknown'
+            , 'source_dec': '0.0'
+            , 'PI_name': 'unknown'
+            , 'proposal_code': u'GBT09A-001'
+            , 'source_ra': '0.0'
+             }
+
+       doc = co.getXMLDoc(info)
+       docStr = etree.tostring(doc)
+       exp = '<currently_observing telescope="GBT"><proposal_code>GBT09A-001</proposal_code><proposal_title>my title</proposal_title><proposal_abstract>my abstract</proposal_abstract><PI_name>unknown</PI_name><PI_institution>unknown</PI_institution><source_name>srcname</source_name><source_ra>0.0</source_ra><source_dec>0.0</source_dec></currently_observing>'
+       self.assertEqual(exp, docStr)
+
+       self.assertTrue(co.validate(doc))
